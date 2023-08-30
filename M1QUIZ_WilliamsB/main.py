ebd435e3c94d420b0a266ca011602a6d49952928
Williamb2158 <williamb2158@student.faytechcc.edu>
2023-08-30 11:12:23 -0400

    M1QUIZ_WilliamsB
    (python turtle program initials)

---
+++ M1QUIZ_WilliamsB/main.py
@@ -0,0 +1,64 @@
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily in Python.
+
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+
+CSC 134
+M1QUIZ
+WilliamsB
+08/30/2023
+"""
+# Draw a shape with turtle graphics
+import turtle
+
+t = turtle.Turtle()
+
+t.color('green')
+t.pensize(5)
+t.penup()
+t.goto(-200,-100)
+t.pendown()
+t.circle(50,180)
+t.right(180)
+t.circle(50,200)
+t.left(70)
+t.forward(200)
+t.penup()
+t.left(90)
+t.forward(100)
+t.pendown()
+t.left(75)
+t.forward(200)
+t.right(140)
+t.forward(90)
+t.left(135)
+t.forward(85)
+t.right(140)
+t.forward(200)
+t.left(75)
+t.penup()
+t.forward(50)
+t.pendown()
+t.left(108)
+t.forward(200)
+t.right(180)
+t.penup()
+t.forward(200)
+t.pendown()
+t.left(135)
+t.forward(75)
+t.right(135)
+t.forward(75)
+t.left(135)
+t.forward(200)
+
+
+
+
+
+
+
+
+
