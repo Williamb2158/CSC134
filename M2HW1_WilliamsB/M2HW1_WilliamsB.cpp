9b4dc30ea9bb96443303aa947c2f87f573702e22
Williamb2158 <williamb2158@student.faytechcc.edu>
2023-09-13 11:50:22 -0400

    M2HW1_WilliamsB
    C++ program for multiple questions. (Will hold multiple sections of code)

---
+++ M2HW1_WilliamsB/M2HW1_WilliamsB.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <iomanip>
+#include <functional>
+using namespace std;
+
+/*
+
+CSC 134
+M2HW1 -
+Brian M. Williams
+09/13/2023
+
+Question 1:
+ - Write a program that simulates banking transactions:
+    - Ask user for their name
+        - Ask user for the following information (all units in dollars)
+            - Starting account balance
+            - Amount of deposit
+            - Amount of withdrawal
+
+Question 2:
+ - Use M2LAB1 program as base
+    - General Crates has updated cost per cubic foot is now 0.3
+    - Management determined they cannot raise their charge per cubic foot above 0.52
+        - Modify the program to match these new conditions
+
+Question 3:
+ - You are throwing a pizza party.
+    - Each visitor should get three slices of pizza.
+        - Ask how many pizzas to order
+        - Ask how many slices per pizza
+        - Ask how many visitors are coming
+            - Calculate and display how many pieces of pizza are left over
+
+Question 4:
+ - Write a cheering program.
+    - Output this message:
+        - "Let's go FTCC"
+          "Let's go FTCC"
+          "Let's go FTCC"
+          "Let's go Trojans"
+    - Use the following variables in cout statement:
+        - string school, team;
+    - For bonus:
+        - A. You can't print out any raw strings ["That looks like this"]
+        - B. You can only use these string variables:
+            - string letsGo, school, team, cheerOne, cheerTwo;
+        - C. You must use string concatenation to build strings cheerOne and cheerTwo
+            - Ex: [string fullName = firstName + " " + lastName
+
+*/
+
+int main() {
+
+    cout << "Question 1:" << endl;
+
+    string fname;
+    string lname;
+    double starting_account_balance;
+    double amount_deposited;
+    double amount_withdrawn;
+
+    cout << "Please enter your first name." << endl;
+    getline(cin, fname);
+    cout << "Please enter your last name." << endl;
+    getline(cin, lname);
+
+    struct S
+    {
+        string
+    };
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
