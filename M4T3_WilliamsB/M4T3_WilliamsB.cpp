7aceab77c3be4bb0b97da1064903cc41d09f5658
Williamb2158 <williamb2158@student.faytechcc.edu>
2023-10-09 11:35:32 -0400

    Updates and finalized M4T3_WilliamsB.cpp
    Finished the program with a menu for the three different programs within the application. Each program also has input validation.

--- M4T3_WilliamsB/M4T3_WilliamsB.cpp
+++ M4T3_WilliamsB/M4T3_WilliamsB.cpp
@@ -22,21 +22,152 @@ The code must be properly indented and commented.
 
 */
 
-
+void part1();
+void part2();
+void part3();
 int main()
 {
-    const int num_days = 5;
-    cout << "Part 1: Counting loop with for" << endl;
-    //TODO
+    string part_selection = "";
 
-    cout << "Part 2: Count with while" << endl;
-    //TODO: ask the user how many cars they saw
-    // each day, then add them up
 
-    int i = 0;
-    while (i < num_days)
-    {
-        cout << "Day#" << i << endl;
-        i = i + 1;
-    }
+   while (!cin.fail())
+   {
+       cout << "Press 1 for Part 1, 2 for Part 2, or any other key for Part 3. Press any other key to quit." << endl;
+       getline(cin, part_selection);
+
+        if (part_selection == "1")
+        {
+            part1();
+        }
+        else if (part_selection == "2")
+        {
+            part2();
+        }
+        else if (part_selection == "3")
+        {
+            part3();
+        }
+        else
+        {
+            break;
+        }
+
+   }
+
+return 0;
 }
+
+void part1()
+{
+    cout << "Part 1:(Using For Loop) Counting the number of cars seen over five days and find the average number of cars seen per day." << endl;
+
+    int number_days = 5;
+    int cars_that_day;
+    int total_cars = 0;
+    for (int i = 1; i <= number_days; i++)
+    {
+        cout << "Enter the number of cars you saw on day#" << i << endl;
+        cin >> cars_that_day;
+
+        if (!cin.fail())
+        {
+            total_cars += cars_that_day;
+        }
+        else
+        {
+            cin.clear();
+            string trash;
+            cin >> trash;
+            i--;
+            cout << "Please enter a number that is zero or larger." << endl;
+        }
+    }
+
+    double average_cars_per_day = (double) total_cars / number_days;
+
+        cout << "The total number of cars seen was: " << total_cars << endl;
+        cout << endl;
+        cout << "The average number of cars seen per day was: " << average_cars_per_day << endl;
+}
+void part2()
+{
+    cout << "Part 2:(Using While Loop) Counting the number of cars seen over five days and find the average number of cars seen per day." << endl;
+
+    int number_days = 5;
+    int cars_that_day;
+    int total_cars = 0;
+    double average_cars_per_day = 0.0;
+    string quit;
+
+    int i = 1;
+    while (i <= number_days)
+    {
+        cout << "Enter the number of cars you saw on Day#" << i << endl;
+        cin >> cars_that_day;
+
+        if (!cin.fail())
+        {
+            total_cars += cars_that_day;
+            i++;
+        }
+        else
+        {
+            cin.clear();
+            string trash;
+            cin >> trash;
+            cout << "Please enter a number that is zero or larger." << endl;
+        }
+    }
+
+        average_cars_per_day = (double) total_cars / number_days;
+
+        cout << "The total number of cars seen was: " << total_cars << endl;
+        cout << endl;
+        cout << "The average number of cars seen per day was: " << average_cars_per_day << endl;
+
+}
+
+void part3()
+{
+    cout << "Part 3:(Sentinel Controlled Loop) Gathers numbers from the user until sentinel value (-1) is " << endl;
+    cout << "entered. Calculate the sum and average of numbers." << endl;
+    cout << endl;
+
+    int user_entry;
+    int total_of_entries = 0;
+    double average_of_entries = 0.0;
+    int number_of_entries = 0;
+    bool done = false;
+    cout << "Enter a group of numbers, hitting the enter key after each number, and entering -1 to stop adding numbers." << endl;
+
+    while (!done)
+    {
+        cin >> user_entry;
+
+        if (!cin.fail() && user_entry != -1)
+        {
+            number_of_entries++;
+            total_of_entries += user_entry;
+        }
+        else if (user_entry == -1)
+        {
+            cout << "Program finished. Now calculating the total and average." << endl;
+            done = true;
+        }
+        else
+        {
+            cin.clear();
+            string trash;
+            cin >> trash;
+            cout << "Please enter an integer value to be added to the group of numbers or -1 to quit." << endl;
+        }
+    }
+
+    average_of_entries = (double) total_of_entries / number_of_entries;
+
+    cout << "The total sum of your entered values equals: " << total_of_entries << endl;
+    cout << endl;
+    cout << "The average of your entered values equals: " << average_of_entries << endl;
+
+}
+
